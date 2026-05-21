0eb4be0c8700e37bc4d6234a84bd6ada43f91017
Luca Marini <lucamarinio20@gmail.com>
2026-05-21 11:26:05 -0300

    refactor(profile): elimina binding redundante de ProfileRepository (S-2)

--- src/app/features/profile/ui/profile.routes.ts
+++ src/app/features/profile/ui/profile.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProfileRepository } from '../domain/profile.repository';
-import { DexieProfileRepository } from '../data/dexie-profile.repository';
 import { GetUserProfileUseCase } from '../domain/use-cases/get-user-profile.use-case';
 import { SetUserProfileUseCase } from '../domain/use-cases/set-user-profile.use-case';
 
@@ -9,10 +7,6 @@ export const PROFILE_ROUTES: Routes = [
     path: '',
     loadComponent: () =>
       import('./pages/profile.page').then((m) => m.ProfilePage),
-    providers: [
-      { provide: ProfileRepository, useClass: DexieProfileRepository },
-      GetUserProfileUseCase,
-      SetUserProfileUseCase,
-    ],
+    providers: [GetUserProfileUseCase, SetUserProfileUseCase],
   },
 ];
